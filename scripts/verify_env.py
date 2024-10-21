bca02cf40f62d52e8cd1d9b6d8faf942c01fa0e5
onurdenizs <onurdnz@gmail.com>
2024-10-21 19:37:20 +0300

    verify_env.py add

---
+++ scripts/verify_env.py
@@ -0,0 +1,9 @@
+# verify_env.py
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+print("All libraries imported successfully!")
